ad8a0640564ac0f476540c1a2fe86c0ea7421a45
Saber_Ben_Hamouda <benhamoudasabeur@gmail.com>
2024-06-27 13:21:55 +0200

    :sparkles: ability to show more images and add search by key Press

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
 
   const [photos,setPhotos] = useState([]);
+  const [imagesPerPage,setImagePerPage] = useState(7);
   const [inputValue,setinputValue]=useState('');
   const handleInput =(e) =>{
     setinputValue(e.target.value);
@@ -11,35 +12,41 @@ function App() {
 
   async function imageGenerator(){
     
-    const urlApi ="https://pixabay.com/api/?&image_type=photo&pretty=true&per_page=7";
+    const urlApi ="https://pixabay.com/api/?&image_type=photo&pretty=true";
     const keyApi = "44615372-6c9ab44c6c3133d5cb157b0b3";
     
     
     try{
-      const response = await fetch(urlApi + `&q=${inputValue}` + `&key=${keyApi}`);
-      console.log(response);
-      if(!response.ok){
-        alert("error");
-      }
-      let images = await response.json();
-
-      setPhotos(images.hits);
-      return images;
+      fetch(urlApi + `&q=${inputValue}` + `&key=${keyApi}` + `&per_page=${imagesPerPage}`)
+      
+      .then(response =>{
+        return response.json();
+      })
+      .then(images => {
+        setPhotos(images.hits);
+      })
       
     }
     catch (error){
       console.error('Error',error);
     };
   }
-  //console.log(imageGenerator());
+  function generateMoreImages(){
+    setImagePerPage(imagesPerPage + 5)
+    imageGenerator();
+  }
+
+  const handleKey = (event) =>{
+    if(event.key === 'Enter'){
+      imageGenerator();
+    }
+  }
   
   const mapper = (element) => (
     <img src={element.webformatURL}/>
   )
 
   const mappedImages = photos.map(mapper);
-  
-  
 
   return (
     <div >
@@ -47,11 +54,11 @@ function App() {
       <h1> Image Search Engine</h1>
 
       <div className='search'>
-        <input type='texte' className='search-box' onChange={handleInput} placeholder='Search anything here ...'/>
+        <input type='texte' className='search-box' onKeyUp={handleKey} onChange={handleInput} placeholder='Search anything here ...'/>
         <button className='search-btn' onClick={imageGenerator}> search </button>
       </div>
       <div id='search-result'> {mappedImages} </div>
-      <button id='show-more-btn'> show more </button>
+      <button id='show-more-btn' onClick={generateMoreImages}> show more </button>
     </div>
   );
 }
